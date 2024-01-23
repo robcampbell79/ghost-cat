0a4432927d4bd42cc32876d84079295454aa2fa3
Robert Campbell <robcampbell@ucmo.edu>
2024-01-23 16:25:00 -0600

    Add a toml file, pull info from toml file, add html file to display

--- main.go
+++ main.go
@@ -2,8 +2,30 @@ package main
 
 import(
 	"fmt"
+	"net/http"
+
+	"github.com/BurntSushi/toml"
 )
 
+type Server struct {
+	host 		string `toml:"server"`
+	port 		string `toml:"port"`
+	indexDir 	string `toml:"indexDir"`
+}
+
 func main() {
-	fmt.Println("This is just a formality")
+	var srv Server
+
+	_, err := toml.DecodeFile("conf.toml", &srv)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fs := http.FileServer(http.Dir("www"))
+
+	fmt.Println("GhostCat is running ... BOO!")
+
+	http.Handle("/", fs)
+
+	http.ListenAndServe(":8081", nil)
 }
